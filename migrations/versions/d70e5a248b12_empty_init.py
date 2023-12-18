88df8cd133a155433016408896981fa322e89631
Samuel Gichuhi <gichuhi1sam@gmail.com>
2023-12-18 23:57:15 +0300

    Completed Code-Along

---
+++ migrations/versions/d70e5a248b12_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: d70e5a248b12
+Revises: 
+Create Date: 2023-12-18 23:46:19.308353
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd70e5a248b12'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
